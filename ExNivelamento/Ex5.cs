4ec271f7ffa11187eafe21b5c2bb4758c7579f8e
Miguel Carvalho <121458728+MiguelMartins71@users.noreply.github.com>
2026-03-08 01:13:01 -0300

    Add files via upload

---
+++ ExNivelamento/Ex5.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExNivelamento
+{
+    internal class Ex5
+    {
+
+        static void Main()
+        {
+
+            int anterior = int.MinValue;
+            int seqAtual = 0;
+            int maiorSeq = 0;
+
+            while (true)
+            {
+                int num = int.Parse(Console.ReadLine());
+
+                if (num == -1)
+                    break;
+
+                if (num > anterior)
+                {
+                    seqAtual++;
+                }
+                else
+                {
+                    seqAtual = 1;
+                }
+
+                if (seqAtual > maiorSeq)
+                {
+                    maiorSeq = seqAtual;
+                }
+
+                anterior = num;
+            }
+
+            Console.WriteLine("Maior sequência crescente: " + maiorSeq);
+        }
+    }
+}
---
+++ ExNivelamento/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExNivelamento
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite");
+            int N = int.Parse(Console.ReadLine());
+
+            int c = 0;
+
+            for (int i = 1; i <= N; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    c++;
+                    Console.Write(c + " ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
