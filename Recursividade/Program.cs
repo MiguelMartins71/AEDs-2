964ac880d4f30d915046c64c89677b41011b9e63
Miguel Carvalho <121458728+MiguelMartins71@users.noreply.github.com>
2026-03-13 20:03:05 -0300

    Add files via upload

---
+++ Recursividade/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursividade
+{
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int numero = fat(5);
+            Console.WriteLine("Fatorial de 5= "+numero);
+     
+            
+        }
+
+        public static int fat(int n)
+        {
+            int res;
+            if(n < 1)
+            {
+                res = 1;
+            }
+            else
+            {
+
+               res = n * fat(n - 1);
+            }
+            return res;
+
+            
+        }
+
+    }
+
+
+    }
